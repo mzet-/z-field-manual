6802f4338e9a14a88b417dd886824e524f7fdba4
mzet <mzet-@users.noreply.github.com>
2020-04-08 13:21:35 +0200

    Update observer.sh

--- scripts/observer.sh
+++ scripts/observer.sh
@@ -5,7 +5,7 @@
 # already discovered services
 DIR=$1
 KNOWN_PORTS='allPorts.txt'
-TMP_SRV_FILE='/tmp/allPortsNow.txt'
+TMP_PORT_FILE='/tmp/allPortsNow.txt'
 
 [ -f "$TMP_PORT_FILE" ] && rm "$TMP_PORT_FILE"
 
@@ -28,7 +28,7 @@ done
 echo; echo "Discovered new ports: "
 
 # store ports that not have yet been seen previously
-DIFF=$(grep -v -f "$KNOWN_PORTS" "$TMP_PORT_FILE")
+DIFF="$(grep -v -f "$KNOWN_PORTS" "$TMP_PORT_FILE")"
 
-# if new ports have been discoverd store it in delta file
-[ -n "$DIFF" ] && echo "$DIFF" | tee vscans/newPorts-delta-$(date +%F_%H:%M).incremental >> $KNOWN_PORTS
+# if new ports have been discoverd store it in delta file and append to KNOWN_PORTS
+[ -n "$DIFF" ] && (echo "$DIFF" | tee -a $KNOWN_PORTS | tee vscans/newPorts-delta-$(date +%F_%H:%M).incremental)
