e0a83efc07ff790327a621221b4630f1916ae66a
mzet <mzet@owasp.org>
2021-06-11 18:02:45 +0200

    update

--- scripts/tlsScrape.sh
+++ scripts/tlsScrape.sh
@@ -3,8 +3,11 @@
 TARGETS="$1"
 PORT=443
 
-# if file "$TARGETS" exists use its content as a target specification otherwise treat input as a cidr
-if [ -f "$TARGETS" ]; then
+# if file "$TARGETS" exists use its content as a target specification. If no CIDR entry is in the file treat is as a IP list and skip active scanning phase.
+if [ -f "$TARGETS" -a ! "$(grep '/' "$TARGETS")" ]; then
+    IPs="$(cat "$TARGETS")"
+# if file "$TARGETS" exists use its content as a target specification otherwise treat input as a single CIDR
+elif [ -f "$TARGETS" ]; then
     IPs="$(masscan -oL - -iL "$TARGETS" -p "$PORT" 2>/dev/null | grep -v "^#.*" | cut -d' ' -f4)"
 else
     IPs="$(masscan -oL - "$TARGETS" -p "$PORT" 2>/dev/null | grep -v "^#.*" | cut -d' ' -f4)"
